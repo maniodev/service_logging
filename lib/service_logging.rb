af8083239a6da5a8ae3ed1c23603c4f71bac5a09
Timur Khafizov <timur.khafizov@savedo.de>
2017-04-04 15:20:57 +0200

    Add initializer

--- lib/service_logging.rb
+++ lib/service_logging.rb
@@ -31,6 +31,26 @@ module ServiceLogging
 
     self.filters = app.config.service_logging.filters || {}
     self.enabled = true
+
+    ActionDispatch::DebugExceptions.class_eval do
+      alias_method :old_log_error, :log_error
+
+      def log_error(request, wrapper)
+        exception = wrapper.exception
+        if exception.is_a?(ActionController::RoutingError)
+          data = {
+            method: request.method,
+            path: request.original_fullpath,
+            status: wrapper.status_code,
+            error: "#{exception.class.name}: #{exception.message}"
+          }
+          formatted_message = Lograge.formatter.call(data)
+          logger(request).send(Lograge.log_level, formatted_message)
+        else
+          old_log_error(request, wrapper)
+        end
+      end
+    end
   end
 
   def custom_options_callback
